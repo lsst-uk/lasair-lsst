9d5e9c3dca11912f9cd97560bcc38a239b4e501d
G Francis <g.francis@epcc.ed.ac.uk>
2023-07-06 11:56:34 +0000

    add test cases

--- tests/integration/example/test_example.py
+++ tests/integration/example/test_example.py
@@ -25,28 +25,42 @@ class ExampleIntegrationTest(TestCase):
     def setUpClass(cls):
         """Set up connection and ensure that the test table exists."""
         cls.connection = pymysql.connect(
-                host = conf.host,
-                user = conf.user,
-                password = conf.password,
-                db = conf.db,
-                charset = conf.charset,
+                host = conf['host'],
+                user = conf['user'],
+                password = conf['password'],
+                db = conf['db'],
+                charset = conf['charset'],
                 cursorclass=pymysql.cursors.DictCursor)
-        query = f"CREATE TABLE IF NOT EXISTS { conf.table }( 'id' int NOT NULL, 'foo' float, PRIMARY KEY (id) )"
-        try:
-            with cls.connection.cursor() as cursor:
-                cursor.execute(query)
+       # 'id' int NOT NULL, 'foo' float, PRIMARY KEY (id) ) 
+        query = f"CREATE TABLE IF NOT EXISTS { conf['table'] }( id int NOT NULL, foo float, PRIMARY KEY (id) )"
+        with cls.connection.cursor() as cursor:
+            cursor.execute(query)
 
     @classmethod
     def tearDownClass(cls):
         """Get rid of the test table and tear down connection"""
-        query = f"DROP TABLE { conf.table }"
-        try:
-            with cls.connection.cursor() as cursor:
-                cursor.execute(query)
+        query = f"DROP TABLE { conf['table'] }"
+        with cls.connection.cursor() as cursor:
+            cursor.execute(query)
         cls.connection.close()
 
-    def test_1_do_nothing(self):
-        pass
+    def test_1_write(self):
+        """Write something to the database"""
+        query = f"INSERT INTO { conf['table'] } ( id, foo ) VALUES ( 1, 0.123 )"
+        with ExampleIntegrationTest.connection.cursor() as cursor:
+            count = cursor.execute(query)
+            self.assertEqual(count, 1)
+        
+    def test_2_read(self):
+        """Read something from the database"""
+        query = f"SELECT * FROM { conf['table'] } WHERE id=1"
+        with ExampleIntegrationTest.connection.cursor() as cursor:
+            count = cursor.execute(query)
+            self.assertEqual(count, 1)
+            result = cursor.fetchone()
+            self.assertEqual(result.get('id'), 1)
+            self.assertEqual(result.get('foo'), 0.123)
+        
 
 if __name__ == '__main__':
     import xmlrunner
