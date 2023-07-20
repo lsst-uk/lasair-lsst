25dd4e9626809b7ff735ea1fd9e3e7ff3837f693
Roy Williams <roy@roe.ac.uk>
2023-07-20 14:35:59 +0000

    calling run_query

--- tests/integration/pipeline/run_query_test.py
+++ tests/integration/pipeline/run_query_test.py
@@ -6,10 +6,14 @@ Can be used as a template for writing integration tests.
 Requires a functional database running on localhost.
 """
 
+import sys
 import unittest.main
 from unittest import TestCase, expectedFailure
 import mysql.connector
 from mysql.connector.errors import *
+sys.path.append('../../../pipeline/filter')
+from run_active_queries import run_query, send_email
+sys.path.append('../../../common')
 
 config = {
         'user':     'ztf',
@@ -21,7 +25,14 @@ config = {
         'charset':  'utf8mb4',
         }
 
-table = 'example123'
+table = 'test_objects'
+
+querydict = {
+        'real_sql'   : None,
+        'topic_name' : 'test_topic',
+        'active'     : None,
+        'email'      : 'roydavidwilliams@gmail.com'
+}
 
 class RunQueryTest(TestCase):
 
@@ -29,7 +40,8 @@ class RunQueryTest(TestCase):
     def setUpClass(cls):
         """Set up connection and ensure that the test table exists."""
         cls.msl = mysql.connector.connect(**config)
-        query = f"CREATE TABLE IF NOT EXISTS { table }( id int NOT NULL, foo float, PRIMARY KEY (id) )"
+        query = f"CREATE TABLE IF NOT EXISTS { table } "
+        query += "( objectId varchar(16) NOT NULL, RA float, PRIMARY KEY (objectId) )"
         with cls.msl.cursor(buffered=True, dictionary=True) as cursor:
             cursor.execute(query)
 
@@ -43,36 +55,32 @@ class RunQueryTest(TestCase):
 
     def test_1_write(self):
         """Write something to the database"""
-        query = f"INSERT INTO { table } ( id, foo ) VALUES ( 1, 0.123 )"
+        query = f"INSERT INTO { table } ( objectId, RA ) VALUES ( 'ZTF23abcdef', 23.123 )"
         with RunQueryTest.msl.cursor(buffered=True, dictionary=True) as cursor:
             cursor.execute(query)
-            # Gareth -- cursor doesn't return anything. The test here is no exception
         
-    def test_2_read(self):
+    def test_2_run_query(self):
         """Read something from the database"""
-        query = f"SELECT * FROM { table } WHERE id=1"
-        with RunQueryTest.msl.cursor(buffered=True, dictionary=True) as cursor:
-            cursor.execute(query)
-            row = cursor.fetchone()
-            self.assertEqual(row.get('id'), 1)
+        query = f"SELECT * FROM { table } WHERE RA BETWEEN 23 and 24"
+        querydict['real_sql'] = query
+        query_results = run_query(querydict, RunQueryTest.msl)
+        print(query_results)
 
     def test_3_syntax_err(self):
         """What happens with query syntax error"""
         query = f"SELECT * FROM { table } WWWWWWWWWWWWWWWWWWWWHERE id=1"
+        querydict['real_sql'] = query
         with RunQueryTest.msl.cursor(buffered=True, dictionary=True) as cursor:
-            try:
-                result = cursor.execute(query)
-            except ProgrammingError as e:
-                print('syntax error')
+            query_results = run_query(querydict, RunQueryTest.msl)
+            print(query_results)
 
     def test_4_timeout_err(self):
         """Timeout error"""
         query = f"SET STATEMENT max_statement_time=2 FOR SELECT SLEEP(4)"
+        querydict['real_sql'] = query
         with RunQueryTest.msl.cursor(buffered=True, dictionary=True) as cursor:
-            try:
-                result = cursor.execute(query)
-            except DatabaseError as e:
-                print('timeout error')
+            query_results = run_query(querydict, RunQueryTest.msl)
+            print(query_results)
 
 if __name__ == '__main__':
     import xmlrunner
