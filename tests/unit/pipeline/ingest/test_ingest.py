f491b29d33e841d34a3476a4c6c58f7f6740677a
Roy Williams <roy@roe.ac.uk>
2023-11-30 16:10:46 +0000

    add unit tests for ingest

---
+++ tests/unit/pipeline/ingest/test_ingest.py
@@ -0,0 +1,42 @@
+import unittest, unittest.mock
+
+import signal
+import subprocess
+
+import context
+import ingest
+
+import settings
+import fastavro
+import pickle
+import io
+import json
+
+class SherlockWrapperConsumerTest(unittest.TestCase):
+
+    # check that the sigterm handler sets sigterm raised correctly
+    def test_sigterm_handler(self):
+        self.assertFalse(ingest.sigterm_raised)
+        subprocess.run(['pkill', '-f', 'python3 test_ingest.py'])
+        self.assertTrue(ingest.sigterm_raised)
+
+    # test using the image store 
+    def test_store_iamges(self):
+        # load up a test alert
+        with open('msg.bin', 'rb') as f:
+            lsst_alert = pickle.load(f)
+        diaSourceId = lsst_alert['diaSource']['diaSourceId']
+        imjd = int(lsst_alert['diaSource']['midPointTai'])
+        # use a mock object for the image store
+        with unittest.mock.MagicMock() as mock_image_store: 
+            result = ingest.store_images(lsst_alert, mock_image_store, diaSourceId, imjd)
+            # check we returned something other than None
+            self.assertIsNotNone(result)
+            # check we called putObject twice
+            self.assertEqual(mock_image_store.putObject.call_count, 2)
+
+if __name__ == '__main__':
+    import xmlrunner 
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    unittest.main()
