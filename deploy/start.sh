7dbbd44ed1e130fde5fb62778cdc805cea4cc3f4
Roy Williams <roy@roe.ac.uk>
2023-11-30 09:45:34 +0000

    fix start/stop commands

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,2 +1,6 @@
-ansible-playbook control.yaml --extra-vars "command=start" --tags=$1
+if [ $1 ]; then
+	ansible-playbook control.yaml --extra-vars "command=start" --tags=$1
+else
+	ansible-playbook control.yaml --extra-vars "command=start" --tags=$1
+fi
 
