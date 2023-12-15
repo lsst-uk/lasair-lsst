53a192d86d12c1302d05a553b24ce4fa2395e71f
Roy Williams <roydavidwilliams@gmail.com>
2023-12-15 11:26:54 +0000

    batched tap

--- utility/DP02/tap.py
+++ utility/DP02/tap.py
@@ -1,7 +1,17 @@
+""" tap.py
+Code to pull down DP0.2 from the Rubin RSP using the API.
+It takes a range of Dec, for example decMin=-30.1 ro DecMax=-30.0
+where DP0.2 is from about -45 to -25
+It gets all the diaObjects between the limits, 
+then all the diaSources after MJD=61000, and all the forced sources after MJD=61000
+Note the finicky join to get the time of the forced source.
+You will need a RSP token, whhc should go into a settings.py file that has a line
+RSP_TOKEN = 'xxxx'
+"""
 import sys, os, requests, time, settings, json, gzip
 import pyvo as pyvo
 import numpy as np
-from astropy.io import ascii
+import settings
 
 url = 'https://data.lsst.cloud/api/tap'
 
@@ -31,7 +41,8 @@ def getDiaSource(diaObjectIdList):
     query = """
     SELECT *
     FROM dp02_dc2_catalogs.DiaSource
-    WHERE diaObjectId in (%s)""" % ','.join([str(d) for d in diaObjectIdList])
+    WHERE midPointTai>61000 AND
+    diaObjectId in (%s)""" % ','.join([str(d) for d in diaObjectIdList])
     results = service.search(query)
     DiaSrcs = results.to_table()
     del results
@@ -42,7 +53,8 @@ def getForcedSourceOnDiaObject(diaObjectIdList):
     dp02_dc2_catalogs.ForcedSourceOnDiaObject as fs 
     JOIN dp02_dc2_catalogs.CcdVisit as cv 
     ON cv.CcdVisitId=fs.CcdVisitId 
-    WHERE fs.diaObjectId IN (%s)""" % ','.join([str(d) for d in diaObjectIdList])
+    WHERE cv.expMidptMJD>61000 AND
+    fs.diaObjectId IN (%s)""" % ','.join([str(d) for d in diaObjectIdList])
     results = service.search(query)
     DiaSrcs = results.to_table()
     del results
@@ -69,21 +81,27 @@ def getBatch(diaObjectList, fileOut):
             'DiaSourceList'              : [s for s in  sources if s['diaObjectId']==diaObjectId],
             'ForcedSourceOnDiaObjectList': [f for f in fsources if f['diaObjectId']==diaObjectId],
         }
-        objList.append(obj)
+        # don't want objects with no detections
+        if len(obj['DiaSourceList']) > 0:
+            objList.append(obj)
 
     s = json.dumps(objList, default=np_encoder)
     with gzip.open(fileOut, 'wt', encoding='UTF-8') as zipfile:
         zipfile.write(s)
 
-if __name__ == '__main__':
-    batchSize = 50
-    if len(sys.argv) > 2:
-        decMin        = float(sys.argv[1])
-        decMax        = float(sys.argv[2])
-
-    dirOut = 'data/data_%.4f_%.4f' % (-decMin, -decMax)
+#############################################
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print('Usage: tap.py <directory> <decMin> <decMax> ')
+        sys.exit()
+    else:
+        decMin    = float(sys.argv[2])
+        decMax    = float(sys.argv[3])
+        dirOut = sys.argv[1] + '/data_%.4f_%.4f' % (-decMin, -decMax)
         
-    print('Data going to ', dirOut)
+    # this batchsize is carefully chosen so the pyvo doesn't fail
+    batchSize = 200
+    print('Data going to %s with dec %f to %f' % (dirOut, decMin, decMax))
     os.system('mkdir -p %s' % dirOut)
 
     t0 = time.time()
