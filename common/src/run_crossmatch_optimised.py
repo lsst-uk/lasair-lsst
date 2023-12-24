193194030fd104b479bdd224239d128e6f4072de
RoyWilliams <roy@roe.ac.uk>
2023-12-24 14:01:13 +0000

    update

--- common/src/run_crossmatch_optimised.py
+++ common/src/run_crossmatch_optimised.py
@@ -114,7 +114,7 @@ def run_crossmatch(msl, radius, wl_id, batchSize=5000, wlMax=False):
             dbSettings=dbSettings
         )
 
-    message = f"{n_hits} ZTF objects have been associated with the {n_cones} sources in this watchlist"
+    message = f"{n_hits} LSST objects have been associated with the {n_cones} sources in this watchlist"
     print(message)
     return n_hits, message
 
