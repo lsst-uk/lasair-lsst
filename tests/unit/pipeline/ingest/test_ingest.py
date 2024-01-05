7cd07c6a05258c681daee19d2e2e3ee244dc3971
G Francis <g.francis@epcc.ed.ac.uk>
2024-01-05 15:29:43 +0000

    add dp02 sample

--- tests/unit/pipeline/ingest/test_ingest.py
+++ tests/unit/pipeline/ingest/test_ingest.py
@@ -15,22 +15,38 @@ class IngestTest(unittest.TestCase):
         subprocess.run(['pkill', '-f', 'python3 test_ingest.py'])
         self.assertTrue(ingest.sigterm_raised)
 
-# Disabling this test for now while cutouts are in a state of flux
-#    # test using the image store 
+    # test using the image store 
     def test_store_images(self):
         # load up a test alert
-        with open('msg.bin', 'rb') as f:
+        with open('alert_single_visit_sample.bin', 'rb') as f:
             lsst_alert = pickle.load(f)
         diaSourceId = lsst_alert['diaSource']['diaSourceId']
         diaObjectId = lsst_alert['diaObject']['diaObjectId']
         imjd = int(lsst_alert['diaSource']['midPointTai'])
-#        # use a mock object for the image store
+        # use a mock object for the image store
         with unittest.mock.MagicMock() as mock_image_store: 
             result = ingest.store_images(lsst_alert, mock_image_store, diaSourceId, imjd, diaObjectId)
             # check we returned something other than None
             self.assertIsNotNone(result)
             # check we called putCutout twice
             self.assertEqual(mock_image_store.putCutout.call_count, 2)
+
+    # test cassandra insert 
+    def test_insert_cassandra(self):
+        # load up a test alert
+        with open('alert_dp02.bin', 'rb') as f:
+            lsst_alert = pickle.load(f)
+            ##print(lsst_alert)
+        alert = {
+            'diaObject':                 lsst_alert['DiaObject'],
+            'diaSourcesList':            lsst_alert['DiaSourceList'],
+            'forcedSourceOnDiaObjectsList':      lsst_alert['ForcedSourceOnDiaObjectList'],
+        }
+        with unittest.mock.patch('gkdbutils.ingesters.cassandra.executeLoad') as mock_executeLoad:
+            mock_executeLoad.return_value = None
+            cassandra_session = None
+            ingest.insert_cassandra(alert, cassandra_session)
+            print (mock_executeLoad.call_count)     
 
 if __name__ == '__main__':
     import xmlrunner 
