c6fc8e8da7d4d5eb85bff582847e58c4e1fbf8ae
G Francis <g.francis@epcc.ed.ac.uk>
2024-03-20 15:54:41 +0000

    add services tests

---
+++ tests/unit/services/test_make_area_files.py
@@ -0,0 +1,14 @@
+import context
+import make_area_files
+import unittest
+
+
+class AnnotationsDumpTest(unittest.TestCase):
+    """Placeholder"""
+
+
+if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    
