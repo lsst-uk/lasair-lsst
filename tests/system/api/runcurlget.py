f178c4319c8153fa0d80a36683bc0d8eb149f323
G Francis <g.francis@epcc.ed.ac.uk>
2024-04-01 13:25:13 +0100

    fix url in api test

--- tests/system/api/runcurlget.py
+++ tests/system/api/runcurlget.py
@@ -6,11 +6,12 @@ sys.path.append('../../../common')
 try:
     import settings
     token = settings.token
+    url = f"https://{settings.LASAIR_URL}/api"
 except:
     token = '4b762569bb349bd8d60f1bc7da3f39dbfaefff9a'
+    url = 'https://lasair-lsst.lsst.ac.uk/api'
 
 out_file = 'tmp.json'
-url = 'https://lasair-iris.roe.ac.uk/api'
 
 
 def curlgettest(input, method, case):
