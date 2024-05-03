4883f4c66e2726aace4c7844b8b4f3dfa0a7adfe
Roy Williams <roy@roe.ac.uk>
2024-05-03 08:26:26 +0000

    schema registry integration

---
+++ common/schema/schema_from_registry.py
@@ -0,0 +1,22 @@
+""" Fetches a schema from the schema registry
+    Currently the registry is https://usdf-alert-schemas-dev.slac.stanford.edu
+    Currently the only working schema_id is 1
+"""
+import os
+import sys
+import time
+import json
+from confluent_kafka.schema_registry import SchemaRegistryClient
+sys.path.append('..')
+import settings
+
+if len(sys.argv) > 1:
+    schema_id = int(sys.argv[1])
+else:
+    schema_id = 1
+
+sr_client = SchemaRegistryClient({"url": settings.SCHEMA_REG_URL})
+schema = sr_client.get_schema(schema_id=schema_id)
+s = json.loads(schema.schema_str)
+print(json.dumps(s, indent=2))
+
