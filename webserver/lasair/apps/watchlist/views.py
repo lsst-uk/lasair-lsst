86a1f26580b27dbbc7cc548004ab84b661b5da71
Roy Williams <roy@roe.ac.uk>
2024-08-05 08:09:22 +0000

    update

--- webserver/lasair/apps/watchlist/views.py
+++ webserver/lasair/apps/watchlist/views.py
@@ -11,7 +11,6 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.template.context_processors import csrf
 from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
 from datetime import timezone
 import src.run_crossmatch_optimised as run_crossmatch
 from django.conf import settings
