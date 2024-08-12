8e78ae3220eae47ad6eb47494218ef745915ec96
G Francis <g.francis@epcc.ed.ac.uk>
2024-08-12 17:20:20 +0100

    fix tests

--- tests/unit/pipeline/sherlock/test_sherlock_runner.py
+++ tests/unit/pipeline/sherlock/test_sherlock_runner.py
@@ -14,24 +14,28 @@ class RunnerTest(unittest.TestCase):
     @patch('sherlock_runner.lasairLogging')
     def test_run_wrapper(self, mock_logging, mock_wrapper):
         """Test that run works"""
-        sherlock_runner.setup_proc(1, 2, 'test_config.json')
+        sherlock_runner.setup_proc(None, [], 1, 2, 'test_config.json')
         mock_wrapper.run.assert_called_once()
         mock_logging.getLogger.return_value.info.assert_called_with('Starting sherlock runner process 1 of 2')
 
     @patch('sherlock_runner.wrapper')
     @patch('sherlock_runner.lasairLogging')
-    def test_run_wrapper_exception(self, mock_logging, mock_wrapper):
+    @patch('sherlock_runner.os.kill')
+    def test_run_wrapper_exception(self, mock_kill, mock_logging, mock_wrapper):
         """Test handling of exception on run"""
+        mock_exit_code = unittest.mock.MagicMock()
         mock_wrapper.run.side_effect = Exception('Test error')
-        # Check that sys.exit is called when an exception happens
-        with self.assertRaises(SystemExit) as cm:
-            sherlock_runner.setup_proc(1, 1, 'test_config.json')
+        # pid needs to be anything except ours
+        pid = os.getpid() + 1
+        # Check that exit code is set when an exception happens
+        sherlock_runner.setup_proc(mock_exit_code, [pid], 1, 1, 'test_config.json')
         # check that the exit code was not 0
-        self.assertNotEqual(cm.exception.code, 0)
-        # check the exception was the expected one
-        mock_logging.getLogger.return_value.exception.assert_called()
+        self.assertNotEqual(mock_exit_code.value, 0)
         # check the exception got logged
+        mock_logging.getLogger.return_value.exception.assert_called()
         mock_logging.getLogger.return_value.critical.assert_called_with('Unrecoverable error in sherlock: Test error')
+        # check SIGTERM got sent
+        mock_kill.assert_called()
 
 
 if __name__ == '__main__':
