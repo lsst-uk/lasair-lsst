9c8ac521a2489f2dfa7cfaaa3e544b6f9ec289ac
G Francis <g.francis@epcc.ed.ac.uk>
2024-10-15 21:46:19 +0100

    fix typo

--- services/make_watchlist_files.py
+++ services/make_watchlist_files.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
     import date_nid, slack_webhook, lasairLogging
     lasairLogging.basicConfig(
         filename='/home/ubuntu/logs/svc.log',
-        slack_channel = getattr(settings, 'SLACK_CHANNEL', None)
+        slack_channel = getattr(settings, 'SLACK_CHANNEL', None),
         webhook=slack_webhook.SlackWebhook(url=settings.SLACK_URL, channel=slack_channel),
         merge=True
     )
