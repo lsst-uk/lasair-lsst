074924ed98e56d2758f8e93fe374d03664b01310
Roy Williams <roy@roe.ac.uk>
2024-10-25 13:33:22 +0000

    delete todays stats

--- utility/lasairStatistics.py
+++ utility/lasairStatistics.py
@@ -15,10 +15,19 @@ def printLasairStatistics(nid):
         v = '%.3f' % status[key]
         print('%20s %20s' % (key, v.rjust(20)))
 
+def delete_nid(nid):
+    print('Statistics for nid=%d' % nid)
+    ms = manage_status()
+    ms.delete(nid)
+    print('Statistics deleted for nid=', nid)
+    
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1: 
         nid = int(sys.argv[1])
+        if len(sys.argv) > 2 and sys.argv[2].strip() == 'delete':
+            delete_nid(nid)
     else:
         nid = date_nid.nid_now()
+
     printLasairStatistics(nid)
 
