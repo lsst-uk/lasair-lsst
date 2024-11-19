b99c2a58a6e45d2ab05ccfcacf6d0f762a391ae6
Dave <d.r.young@qub.ac.uk>
2024-11-19 17:28:41 +0000

    x

--- webserver/staticfiles/gulpfile.js
+++ webserver/staticfiles/gulpfile.js
@@ -243,7 +243,7 @@ gulp.task('copy:dist:js', function() {
 gulp.task('replace_1', function() {
     gulp.src([paths.src.node_modules + '/js9/js9.js']) // Every file allown. 
         .pipe(replace('$(document).scrollTop(0);', ''))
-        .pipe(gulp.dest(paths.src.node_modules + '/js9/js9.js'))
+        .pipe(gulp.dest(paths.src.node_modules + '/js9/'))
 });
 
 // COPY REQUIRED VENDOR MODULES
