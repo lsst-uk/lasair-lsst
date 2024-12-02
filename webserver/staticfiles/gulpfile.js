cffae239b62e932e7413e52ba63f0a2b1e660fba
Dave <d.r.young@qub.ac.uk>
2024-12-02 16:23:10 +0000

    x

--- webserver/staticfiles/gulpfile.js
+++ webserver/staticfiles/gulpfile.js
@@ -243,7 +243,9 @@ gulp.task('copy:dist:js', function() {
 gulp.task('replace_1', function() {
 
     gulp.src([paths.src.node_modules + '/js9/js9.js'])
-        .pipe(replace('this.displayConjq.focus();', ''))
+        .pipe(replace('$(document).scrollTop(0);', ''))
+        .pipe(gulp.dest(paths.src.node_modules + '/js9/'))
+
     return gulp.src([paths.src.node_modules + '/js9/js9.js']) // Every file allown. 
         .pipe(replace('this.displayConjq.focus();', ''))
         .pipe(gulp.dest(paths.src.node_modules + '/js9/'))
