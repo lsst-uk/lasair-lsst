6364c407d180992e4a33de3d423ca2e33dd985a5
Roy Williams <roy@roe.ac.uk>
2025-02-02 14:53:31 +0000

    update

--- common/schema/make_fake.py
+++ common/schema/make_fake.py
@@ -26,43 +26,58 @@ def make_fake_value(field):
         return None
     return v
 
-diaObject = {}
-for field in diaO.schema['fields']:
-    v = make_fake_value(field)
-    if v: diaObject[field['name']] = v
-
-ssObject = {}
-for field in ssO.schema['fields']:
-    v = make_fake_value(field)
-    if v: ssObject[field['name']] = v
-
-ndiaS = 4
-prvDiaSources = []
-for i in range(ndiaS):
-    diaSource = {}
-    for field in diaS.schema['fields']:
+def make_alert():
+    diaObject = {}
+    for field in diaO.schema['fields']:
         v = make_fake_value(field)
-        if v: diaSource[field['name']] = v
-    if i < ndiaS-1:   # last one stands by itself
-        prvDiaSources.append(diaSource)
-
-ndiaF = 3
-prvDiaForcedSources = []
-for i in range(ndiaF):
-    diaForcedSource = {}
-    for field in diaF.schema['fields']:
+        if v: diaObject[field['name']] = v
+    
+    ssObject = {}
+    for field in ssO.schema['fields']:
         v = make_fake_value(field)
-        if v: diaForcedSource[field['name']] = v
-    prvDiaForcedSources.append(diaForcedSource)
+        if v: ssObject[field['name']] = v
+    
+    ndiaS = random.randrange(1, 10)
+    prvDiaSources = []
+    for i in range(ndiaS):
+        diaSource = {}
+        for field in diaS.schema['fields']:
+            v = make_fake_value(field)
+            if v: diaSource[field['name']] = v
+        if i < ndiaS-1:   # last one stands by itself
+            prvDiaSources.append(diaSource)
+    
+    ndiaF = random.randrange(0, 10)
+    prvDiaForcedSources = []
+    for i in range(ndiaF):
+        diaForcedSource = {}
+        for field in diaF.schema['fields']:
+            v = make_fake_value(field)
+            if v: diaForcedSource[field['name']] = v
+        prvDiaForcedSources.append(diaForcedSource)
+    
+    alertId = random.randrange(100000)
+    alert = {
+        'alertId':             alertId,
+        'diaObject':           diaObject,
+        'diaSource':           diaSource,
+        'prvDiaSources':       prvDiaSources,
+        'prvDiaForcedSources': prvDiaForcedSources,
+        'ssObject':            ssObject,
+    }
+    return alert
+    
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        dir = sys.argv[1]
+        n = int(sys.argv[2])
+    else:
+        print('Usage: make_fake <directory> <number>')
+        sys.exit()
 
-alertId = random.randrange(1000)
-alert = {
-    'alertId':             alertId,
-    'diaObject':           diaObject,
-    'diaSource':           diaSource,
-    'prvDiaSources':       prvDiaSources,
-    'prvDiaForcedSources': prvDiaForcedSources,
-    'ssObject':            ssObject,
-}
-
-print(json.dumps(alert, indent=2))
+    for i in range(n):
+        alert = make_alert()
+        alertId = alert['alertId']
+        f = open('%s/file%05d.json' % (dir, alertId), 'w')
+        f.write(json.dumps(alert, indent=2))
+        f.close()
---
+++ utility/json_to_kafka.py
@@ -0,0 +1,43 @@
+import os, sys, json, io, gzip
+import fastavro
+from confluent_kafka import Producer, KafkaError
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        datadir    = sys.argv[1]
+        schemafile = sys.argv[2]
+        topic      = sys.argv[2]
+    else:
+        print('Usage: json_to_kafka.py <dataset> <schemafile> <topic>')
+        sys.exit()
+
+    schema = json.loads(open(schemafile).read())
+    parsed_schema = fastavro.parse_schema(schema)
+
+    conf = {
+        'bootstrap.servers': 'lasair-lsst-dev-kafka:9092',
+        'client.id': 'client-1',
+    }
+    p = Producer(conf)
+
+    n = 0
+    objList = None
+    for file in os.listdir(datadir):
+        if not file.endswith('json'): continue
+        
+        fin = open(datadir +'/'+ file, 'r')
+        json_str = fin.read()
+        fin.close()
+
+        obj = json.loads(json_str)          
+
+        mockfile = io.BytesIO()
+        fastavro.schemaless_writer(mockfile, parsed_schema, obj)
+        p.produce(topic, mockfile.getvalue())
+        n +=1
+        if n%100 == 0: 
+            print(n)
+            p.flush()
+            p = Producer(conf)
+    p.flush()
+    print('%d alerts pushed to topic %s' % (n, topic))
