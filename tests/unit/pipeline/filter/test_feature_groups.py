587b07dd6a0929202a5346d680159cbadba77fe4
G Francis <g.francis@epcc.ed.ac.uk>
2025-02-18 17:49:16 +0000

    fix types in FeatureGroup rather than bodging the test

--- tests/unit/pipeline/filter/test_feature_groups.py
+++ tests/unit/pipeline/filter/test_feature_groups.py
@@ -69,14 +69,14 @@ class FeatureTest(TestCase):
         if 'name' in feature:
           name = schema[feature]['name']
           type = schema[feature]['type']
-          if type == 'string':
-              type = 'str'
-          if name == 'timestamp':
-              continue
+          #if type == 'string':
+          #    type = 'str'
+          #if name == 'timestamp':
+          #    continue
           # check name is in the feature set
           self.assertIn(name, output)
-        # check that either the type is ok or that the output is None and allowed to be so 
-          print('===', name, type, output[name])
+          # check that either the type is ok or that the output is None and allowed to be so 
+          #print('===', name, type, output[name])
           self.assertTrue(
             (isinstance(output[name], eval(type))) or
             (output[name] is None and schema[feature].get('extra') != 'NOT NULL')
