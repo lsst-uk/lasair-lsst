48e03bd7089e8ce857207da8c3a80af94111ee77
Roy Williams <roy@roe.ac.uk>
2025-04-01 13:22:51 +0000

    update

--- tests/unit/pipeline/filter/sample_alerts/make_sample_alert.py
+++ tests/unit/pipeline/filter/sample_alerts/make_sample_alert.py
@@ -1,4 +1,4 @@
-import sys, json
+import sys, json, os
 sys.path.append('../../../../../common/schema/7_4')
 import diaObjects, diaSources
 
@@ -11,37 +11,43 @@ default = {
     'string' : 'u',
 }
 
+def make_it(infile):
+    detections = json.loads(open(infile).read())
+
+    s_obj = diaObjects.schema
+    s_src = diaSources.schema
+    
+    dobj = {}
+    for f in s_obj['fields']:
+        dobj[f['name']] = default[f['type']]
+
+    dsl = []
+    for detection in detections:
+        ds  = {}
+        for f in s_src['fields']:
+            ds[f['name']] = default[f['type']]
+        ds['midpointMjdTai'] = detection['mjd']
+        ds['psfFlux']        = detection['flux']
+        ds['psfFluxErr']     = detection['flux_err']
+        ds['band']           = detection['band']
+        dsl.append(ds)
+
+    d = {}
+    d['diaObject'] = dobj
+    d['diaSourcesList'] = dsl
+    d['diaForcedSourcesList'] = []
+    d['diaNondetectionLimitsList'] = []
+    d['ssObject'] = None
+
+    f = open(infile, 'w')
+    f.write(json.dumps(d, indent=2))
+    f.close
+
+
 if len(sys.argv) > 1:
     infile = sys.argv[1]
+    make_it(infile)
 else:
-    print('Usage: make_sample_alert <input_file>')
-    sys.exit()
-
-detections = json.loads(open(infile).read())
-
-s_obj = diaObjects.schema
-s_src = diaSources.schema
-
-dobj = {}
-for f in s_obj['fields']:
-    dobj[f['name']] = default[f['type']]
-
-dsl = []
-for detection in detections:
-    ds  = {}
-    for f in s_src['fields']:
-        ds[f['name']] = default[f['type']]
-    ds['midpointMjdTai'] = detection['mjd']
-    ds['psfFlux']        = detection['flux']
-    ds['psfFluxErr']     = detection['flux_err']
-    ds['band']           = detection['band']
-    dsl.append(ds)
-
-d = {}
-d['diaObject'] = dobj
-d['diaSourcesList'] = dsl
-d['diaForcedSourcesList'] = []
-d['diaNondetectionLimitsList'] = []
-d['ssObject'] = None
-
-print(json.dumps(d, indent=2))
+    for infile in os.listdir('plasticc'):
+        if infile.endswith('.json'):
+            make_it('plasticc/' + infile)
