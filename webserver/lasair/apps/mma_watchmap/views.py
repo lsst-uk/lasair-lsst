4e972d3997eedeebab8ca0041d1351ad4dd15eee
Roy Williams <roy@roe.ac.uk>
2025-04-19 11:40:48 +0000

    mma_gw and mma_fermi

--- webserver/lasair/apps/mma_watchmap/views.py
+++ webserver/lasair/apps/mma_watchmap/views.py
@@ -118,9 +118,9 @@ def mma_watchmap_detail(request, mw_id):
 SELECT
 o.diaObjectId, 
 h.probdens2, h.contour, 
-o.maxTai as "last detected",
-o.minTai - m.event_tai as "t_GW",
-o.rPSFlux, o.gPSFlux,
+o.lastDiaSourceMJD as "last detected",
+o.firstDiaSourceMJD - m.event_tai as "t_GW",
+o.r_psfFlux, o.g_psfFlux,
 o.ra, o.decl
 FROM mma_area_hits as h, objects AS o, mma_areas AS m
 WHERE m.mw_id={mw_id} AND h.mw_id={mw_id} AND o.diaObjectId=h.diaObjectId
@@ -187,10 +187,10 @@ ORDER BY h.probdens2 DESC LIMIT {resultCap}
 SELECT
 o.diaObjectId, 
 h.probdens3, h.contour, h.distance as dist,
-o.maxTai as "last detected",
-o.minTai - m.event_tai as "t_GW",
+o.lastDiaSourceMJD as "last detected",
+o.firstDiaSourceMJD - m.event_tai as "t_GW",
 s.classification, s.distance, s.z, s.photoZ, s.photoZerr,
-o.rPSFlux, o.gPSFlux,
+o.r_psfFlux, o.g_psfFlux,
 o.ra, o.decl 
 FROM mma_area_hits as h, objects AS o, sherlock_classifications AS s, mma_areas AS m
 WHERE m.mw_id={mw_id} AND h.mw_id={mw_id} 
