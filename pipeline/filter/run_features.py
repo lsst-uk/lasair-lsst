aff8724cac07f6365602202a3638e45249d284c3
Lasair <lasair@lsst.ac.uk>
2025-05-26 09:16:40 +0000

    checked feature test

--- pipeline/filter/run_features.py
+++ pipeline/filter/run_features.py
@@ -2,18 +2,24 @@
 python3 run_features.py ~/lasair-lsst/tests/unit/pipeline/filter/sample_alerts/99999999999.json
 python3 run_features.py ~/lasair-lsst/tests/unit/pipeline/filter/sample_alerts/402778310355976216.json
 """
-import sys, json
+import sys, json, os
 sys.path.append('../../common')
 import settings
 sys.path.append('../../common/schema/' + settings.SCHEMA_VERSION)
 from features.FeatureGroup import FeatureGroup
 
-alert_file = sys.argv[1]
-alert = json.loads(open(alert_file).read())
-lasair_features = FeatureGroup.run_all(alert)
-s = json.dumps(lasair_features, indent=2)
+dir = '/home/ubuntu/lasair-lsst/tests/unit/pipeline/filter/sample_alerts'
 
-out_file   = open(sys.argv[2], 'w')
-out_file.write(s)
-out_file.close()
-
+for file in os.listdir(dir):
+    if not file.endswith('.json'): continue
+    if file.endswith('object.json'): continue
+    name = file.split('.')[0]
+    alert_file = dir +'/'+ name + '.json'
+    out_file   = dir +'/'+ name + '_object.json'
+    print(alert_file)
+    alert = json.loads(open(alert_file).read())
+    lasair_features = FeatureGroup.run_all(alert)
+    s = json.dumps(lasair_features, indent=2)
+    f = open(out_file, 'w')
+    f.write(s)
+    f.close()
