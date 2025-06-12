9151ca659fd66740460dfbca82278dc15ebeb0f6
G Francis <g.francis@epcc.ed.ac.uk>
2025-06-12 11:48:34 +0100

    remove unnecessary try/except and formatting fixes

--- pipeline/sherlock/wrapper.py
+++ pipeline/sherlock/wrapper.py
@@ -16,7 +16,6 @@ import sys
 from urllib.parse import urlparse
 import pymysql.cursors
 from confluent_kafka import Consumer, Producer, KafkaError, KafkaException
-#from mock_sherlock import transient_classifier
 from sherlock import transient_classifier
 from pkg_resources import get_distribution
 
@@ -47,19 +46,21 @@ def consume(conf, log, alerts, consumer):
             elif not msg.error():
                 log.debug("Got message with offset " + str(msg.offset()))
                 alert = json.loads(msg.value())
-                #name = alert.get('objectId', alert.get('candid'))
-                #alerts[name] = alert
                 alerts.append(alert)
                 n += 1
             else:
                 n_error += 1
-                try:
-                    if msg.error().fatal():
-                        log.error(str(msg.error()))
-                        break
-                    log.warning(str(msg.error()))
-                except:
-                    pass
+                # try:
+                #    if msg.error().fatal():
+                #        log.error(str(msg.error()))
+                #        break
+                #    log.warning(str(msg.error()))
+                # except:
+                #    pass
+                if msg.error().fatal():
+                    log.error(str(msg.error()))
+                    break
+                log.warning(str(msg.error()))
                 if conf['max_errors'] < 0:
                     continue
                 elif conf['max_errors'] < n_error:
@@ -70,10 +71,7 @@ def consume(conf, log, alerts, consumer):
         log.log(logging.INFO_, "consumed {:d} alerts".format(n))
         if n > 0:
             n_classified = classify(conf, log, alerts)
-            if n_classified != n:
-                # may be different due to SS alerts
-                #raise Exception("Failed to classify all alerts in batch: expected {}, got {}".format(n, n_classified))
-                logging.info("Classified {} of {} alerts".format(n_classified, n))
+            logging.info("Classified {} of {} alerts".format(n_classified, n))
             n_produced = produce(conf, log, alerts)
             if n_produced != n:
                 raise Exception("Failed to produce all alerts in batch: expected {}, got {}".format(n, n_produced))
@@ -96,8 +94,6 @@ def consume(conf, log, alerts, consumer):
 
 def classify(conf, log, alerts):
     """send a batch of alerts to sherlock and add the responses to the alerts, return the number of alerts classified"""
-    
-    #global alerts
 
     log.debug('called classify with config: ' + str(conf))
   
@@ -140,7 +136,8 @@ def classify(conf, log, alerts):
                             match = json.loads(result.get('crossmatch', {}))
                             for key, value in match.items():
                                 annotations[name][key] = value
-                            log.debug(f"Got crossmatch from cache: { name } { result['class'] } { json.dumps(match, indent=2) }\n")
+                            log.debug("Got crossmatch from cache: {} {} {}\n".format(
+                                name, result['class'], json.dumps(match, indent=2)))
                         except ValueError:
                             log.info("Ignoring cache entry with malformed or missing crossmatch: {}".format(name))
                             continue
@@ -149,7 +146,7 @@ def classify(conf, log, alerts):
             log.debug("Got TypeError reading cache. Entry probably present, but incomplete or malformed. Ignoring.")
         finally:
             connection.close()
-    if len(annotations)>0:
+    if len(annotations) > 0:
         log.info("got {:d} annotations from cache".format(len(annotations)))
 
     # make lists of names, ra, dec
@@ -221,7 +218,7 @@ def classify(conf, log, alerts):
         log.log(logging.INFO_, "not running Sherlock as no remaining alerts to process")
 
     # update cache database
-    if conf['cache_db'] and len(names)>0:
+    if conf['cache_db'] and len(names) > 0:
         connection = pymysql.connect(
                 host=url.hostname,
                 user=url.username,
@@ -239,8 +236,10 @@ def classify(conf, log, alerts):
             values.append("\n ('{}','{}','{}','{}',%s)".format(name, sherlock_version, classification, description))
             crossmatches.append(crossmatch)
         # Syntax for ON DUPLICATE KEY appears to differ between MySQL and MariaDB :(
-        ##query = "INSERT INTO cache VALUES {} AS new ON DUPLICATE KEY UPDATE class=new.class, crossmatch=new.crossmatch".format(",".join(values))
-        query = "INSERT INTO cache VALUES {} ON DUPLICATE KEY UPDATE version=VALUES(version), class=VALUES(class), description=VALUES(description), crossmatch=VALUES(crossmatch)".format(",".join(values))
+        # query = "INSERT INTO cache VALUES {} AS new ON DUPLICATE KEY UPDATE class=new.class,
+        # crossmatch=new.crossmatch".format(",".join(values))
+        query = ("INSERT INTO cache VALUES {} ON DUPLICATE KEY UPDATE version=VALUES(version), class=VALUES(class),\
+                 description=VALUES(description), crossmatch=VALUES(crossmatch)".format(",".join(values)))
         log.info("update cache: {}".format(query))
         try:
             with connection.cursor() as cursor:
@@ -259,7 +258,8 @@ def classify(conf, log, alerts):
         if diaObject:
             name = str(diaObject['diaObjectId'])
             if name in annotations:
-                annotations[name]['annotator'] = "https://github.com/thespacedoctor/sherlock/releases/tag/v{}".format(sherlock_version)
+                annotations[name]['annotator'] = (
+                    "https://github.com/thespacedoctor/sherlock/releases/tag/v{}".format(sherlock_version))
                 annotations[name]['additional_output'] = "http://lasair-lsst.lsst.ac.uk/api/sherlock/object/" + name
                 if 'annotations' not in alert:
                     alert['annotations'] = {}
@@ -295,6 +295,7 @@ def produce(conf, log, alerts):
     log.log(logging.INFO_, "produced {:d} alerts".format(n))
     return n
 
+
 def run(conf, log):
     settings = {
         'bootstrap.servers': conf['broker'],
@@ -312,13 +313,13 @@ def run(conf, log):
 
         batch = 0
         while True:
-            if 'max_batches' in conf and conf['max_batches'] > 0 and batch == conf['max_batches']:
+            if batch == conf.get('max_batches', -1):
                 log.log(logging.INFO_, f"reached max batches at batch { batch }")
                 break
             batch += 1
             alerts = []
             n = consume(conf, log, alerts, consumer)
-            if n==0 and conf['stop_at_end']:
+            if n == 0 and conf['stop_at_end']:
                 break
     except Exception as e:
         log.critical(f"Exception: { type(e).__name__ } { str(e) }")
@@ -333,20 +334,26 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--config', default=None, type=str, help='location of config file')
     parser.add_argument('-b', '--broker', type=str, help='address:port of Kafka broker(s)')
     parser.add_argument('-g', '--group', type=str, default='sherlock-dev-1', help='group id to use for Kafka')
-    parser.add_argument('-e', '--stop_at_end', action='store_true', default=False, help='stop when no more messages to consume')
+    parser.add_argument('-e', '--stop_at_end', action='store_true', default=False,
+                        help='stop when no more messages to consume')
     parser.add_argument('-i', '--input_topic', type=str, help='name of input topic')
     parser.add_argument('-o', '--output_topic', type=str, help='name of output topic')
     parser.add_argument('-n', '--batch_size', type=int, default=1000, help='number of messages to process per batch')
     parser.add_argument('-m', '--max_batches', type=int, default=-1, help='max number of batches to process')
-    parser.add_argument('--max_errors', type=int, default=-1, help='maximum number of non-fatal errors before aborting') # negative = no limit
-    parser.add_argument('-d', '--cache_db', type=str, default='', help='cache database (e.g. mysql://user:pw@host:3306/database)') # empty = don't use cache
-    parser.add_argument('-s', '--sherlock_settings', type=str, default='sherlock.yaml', help='location of Sherlock settings file (default sherlock.yaml)')
+    parser.add_argument('--max_errors', type=int, default=-1,
+                        help='maximum number of non-fatal errors before aborting')  # negative = no limit
+    parser.add_argument('-d', '--cache_db', type=str, default='',
+                        help='cache database (e.g. mysql://user:pw@host:3306/database)')  # empty = don't use cache
+    parser.add_argument('-s', '--sherlock_settings', type=str, default='sherlock.yaml',
+                        help='location of Sherlock settings file (default sherlock.yaml)')
     parser.add_argument('-q', '--quiet', action="store_true", default=None, help='minimal output')
     parser.add_argument('-v', '--verbose', action="store_true", default=None, help='verbose output')
     parser.add_argument('--debug', action="store_true", default=None, help='debugging output')
     parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
-    parser.add_argument('--poll_timeout', type=int, default=30, help='kafka consumer poll timeout in s') # see https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html
-    parser.add_argument('--max_poll_interval', type=int, default=300000, help='kafka max poll interval in ms') # see https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    parser.add_argument('--poll_timeout', type=int, default=30, help='kafka consumer poll timeout in s')
+    # see https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html
+    parser.add_argument('--max_poll_interval', type=int, default=300000, help='kafka max poll interval in ms')
+    # see https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
     parser.add_argument('--ignore_singletons', action="store_true", default=False, help='ignore alerts with <2 sources')
     conf = vars(parser.parse_args())
 
@@ -355,10 +362,10 @@ if __name__ == '__main__':
         try:
             with open(conf['config'], "r") as f:
                 cfg = yaml.safe_load(f)
-                for key,value in cfg.items():
+                for key, value in cfg.items():
                     conf[key] = value
         except IOError as e:
-            print (e)
+            print(e)
 
     # set up a logger
     if conf['quiet']:
@@ -376,7 +383,7 @@ if __name__ == '__main__':
     log = logging.getLogger("sherlock_wrapper") 
 
     # print options on debug
-    log.debug("config options:\n"+json.dumps(conf,indent=2))
+    log.debug("config options:\n"+json.dumps(conf, indent=2))
 
     # check that required options are set
     if not conf.get('broker'):
@@ -390,4 +397,3 @@ if __name__ == '__main__':
         sys.exit(2)
 
     run(conf, log)
-
