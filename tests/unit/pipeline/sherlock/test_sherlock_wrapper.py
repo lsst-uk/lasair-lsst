6b25479d3fd122065921e5529ed708567c9bf238
G Francis <g.francis@epcc.ed.ac.uk>
2025-06-12 12:59:23 +0100

    fix test_max_errors

--- tests/unit/pipeline/sherlock/test_sherlock_wrapper.py
+++ tests/unit/pipeline/sherlock/test_sherlock_wrapper.py
@@ -161,10 +161,8 @@ class SherlockWrapperConsumerTest(unittest.TestCase):
             self.assertEqual(alerts, [])
             # poll should have been called twice with timeout 1
             print('calls:', mock_kafka_consumer.poll.mock_calls)
-            #mock_kafka_consumer.poll.assert_has_calls(
-            #    call()
-            #)
-            self.assertEqual(len(mock_kafka_consumer.poll.mock_calls), 2)
+            # poll should have been called twice (poll.error will also have been called several times)
+            mock_kafka_consumer.poll.assert_has_calls(call(1), call(1))
 
     def test_failed_commit(self):
         """test (non-fatal) failure on commit"""
