f1c6d701038c32bcdf3f87923a915c271e693a98
Roy Williams <roydavidwilliams@gmail.com>
2025-06-18 07:30:05 +0000

    update

--- tests/integration/pipeline/filter/test_transfer.py
+++ tests/integration/pipeline/filter/test_transfer.py
@@ -77,11 +77,11 @@ class RunTransferTest(TestCase):
         query = f"SELECT * FROM { table_to }"
         cursor = cls.msl.cursor(buffered=True, dictionary=True)
         cursor.execute(query)
-        result = cursor.fetchall()
+        result = list(cursor.fetchall())
         print(result)
-        cls.assertEqual(result['objectId'], 'ZTF23abcdef')
-        cls.assertEqual(result['a1'],       1.1)
-        cls.assertEqual(result['a2'],       2.2)
+        cls.assertEqual(result[0], 'ZTF23abcdef')
+        cls.assertEqual(result[1],       1.1)
+        cls.assertEqual(result[2],       2.2)
 
 if __name__ == '__main__':
     import xmlrunner
