5771b177eed5c2ce2070d9d0e8d29e74cf0efec5
Roy Williams <roy@roe.ac.uk>
2025-06-25 13:10:42 +0000

    update

--- utility/lasairStatistics.py
+++ utility/lasairStatistics.py
@@ -6,14 +6,40 @@ sys.path.append('../common')
 from src import date_nid
 from src.manage_status import manage_status
 
+def combine_status(status):
+    # the keys come in as apple_0=123, apple_1=199 etc from the nodes
+    # here we combine them into a list apple = [123,199]
+    keys = sorted(status.keys())
+    new_status = {}
+    for key in keys:
+        if not key in status:
+            continue
+        v = '%.0f' % status[key]
+        # collect values from different nodes
+        if key.endswith('_0'):
+            kl = []
+            root = key[:-2]
+            for i in range(10):
+                other_key = '%s_%d' % (root, i)
+                if other_key in status:
+                    s = int(round(status[other_key], 0))
+                    kl.append(s)
+                    del status[other_key]
+                else:
+                    new_status[root] = str(kl)
+                    break
+        else:
+            s = int(round(status[key], 0))
+            new_status[key] = str(s)
+    return new_status
+
 def printLasairStatistics(nid):
     print('Statistics for nid=%d' % nid)
     ms = manage_status()
     status = ms.read(nid)
-    keys = sorted(status.keys())
-    for key in keys:
-        v = '%.3f' % status[key]
-        print('%20s %20s' % (key, v.rjust(20)))
+    new_status = combine_status(status)
+    for k,v in new_status.items():
+        print('%20s %20s' % (k, v.ljust(20)))
 
 def delete_nid(nid):
     print('Statistics for nid=%d' % nid)
