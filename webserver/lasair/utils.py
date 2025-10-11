ee5d7586f17a175012b60c42eaf7b49af5da98c3
Roy Williams <roy@roe.ac.uk>
2025-10-11 10:21:42 +0000

    updated external links

--- webserver/lasair/utils.py
+++ webserver/lasair/utils.py
@@ -134,7 +134,7 @@ def objjson(diaObjectId, lite=False):
     msl = db_connect.readonly()
     cursor = msl.cursor(buffered=True, dictionary=True)
     if lite:
-        query = 'SELECT nSources, ra, decl, firstDiaSourceMjdTai, lastDiaSourceMjdTai '
+        query = 'SELECT nSources, ra, decl, firstDiaSourceMjdTai, lastDiaSourceMjdTai,glat,ebv '
     else:
         query = 'SELECT * '
     query += 'FROM objects WHERE diaObjectId = %s' % diaObjectId
