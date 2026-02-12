d33bfcc23b0d26cc876b1e27aead85ef63857117
Roy Williams <roy@roe.ac.uk>
2026-02-12 15:08:05 +0000

    update

--- pipeline/filter/filters.py
+++ pipeline/filter/filters.py
@@ -71,7 +71,7 @@ def fetch_queries(msl_remote, ms, nid):
         query_list.append(query_dict)
     return query_list
 
-def run_queries(fltr, query_list, ms, nid):
+def run_queries(fltr, query_list, ms, nid, annotation_list=None):
     """
     When annotation_list is None, it runs all the queries against the local database
     When not None, runs some queires agains a specific object, using the main database
@@ -81,20 +81,17 @@ def run_queries(fltr, query_list, ms, nid):
     for query in query_list:
         n = 0
         t = time.time()
-        query_results = run_query(query, fltr.database)
-        n += dispose_query_results(query, query_results, fltr, ms, nid)
-
-# immediate response to active=2 annotators
-# annotation_list is list of fresh annotations
-#            for ann in annotation_list:
-#                # msl_remote = db_connect.remote()
-#                query_results = run_query(query, fltr.database,
-#                                          ann['annotator'], ann['diaObjectId'], fltr=fltr)
-#                print('fast annotator %s on object %s' % (ann['annotator'], ann['diaObjectId']))
-#                print('results:', query_results)
-#                n += dispose_query_results(query, query_results, fltr)
-
-
+        if annotation_list is None:
+            query_results = run_query(query, fltr.database)
+            n += dispose_query_results(query, query_results, fltr, ms, nid)
+        else:
+            for ann in annotation_list:
+                # msl_remote = db_connect.remote()
+                query_results = run_query(query, fltr.database,
+                                          ann['annotator'], ann['diaObjectId'], fltr=fltr)
+                print('fast annotator %s on object %s' % (ann['annotator'], ann['diaObjectId']))
+                print('results:', query_results)
+                n += dispose_query_results(query, query_results, fltr)
 
         t = time.time() - t
         if n > 0:
@@ -103,31 +100,26 @@ def run_queries(fltr, query_list, ms, nid):
         ntotal += n
     return ntotal
 
-#def query_for_object(query, diaObjectId):
-#    """ modifies an existing query to add a new constraint for a specific object.
-#    We already know this query comes from multiple tables: objects and annotators,
-#    so we know there is an existing WHERE clause. Can add the new constraint to the end,
-#    unless there is an ORDER BY, in which case it comes before that.
-#    Args:
-#        query: the original query, as generated from the Lasair query builder
-#        diaObjectId: the object that is the new constraint
-#    """
-#    tok = query.replace('order by', 'ORDER BY').split('ORDER BY')
-#    query = tok[0] + (' AND objects.diaObjectId=%s ' % str(diaObjectId))
-#    if len(tok) == 2: # has order clause, add it back
-#        query += ' ORDER BY ' + tok[1]
-#    return query
-#
-#
-#def run_query(query, msl, annotator=None, diaObjectId=None, fltr=None):
-#    """run_query. Two cases here:
-#    if annotator=None, runs the query against the local database
-#    if annotator and diaObjectId, checks if the query involves the annotator,
-#        and if so, runs the query for the given object on main database
+def query_for_object(query, diaObjectId):
+    """ modifies an existing query to add a new constraint for a specific object.
+    We already know this query comes from multiple tables: objects and annotators,
+    so we know there is an existing WHERE clause. Can add the new constraint to the end,
+    unless there is an ORDER BY, in which case it comes before that.
+    Args:
+        query: the original query, as generated from the Lasair query builder
+        diaObjectId: the object that is the new constraint
+    """
+    tok = query.replace('order by', 'ORDER BY').split('ORDER BY')
+    query = tok[0] + (' AND objects.diaObjectId=%s ' % str(diaObjectId))
+    if len(tok) == 2: # has order clause, add it back
+        query += ' ORDER BY ' + tok[1]
+    return query
 
-
-def run_query(query, msl):
-    """run_query.
+def run_query(query, msl, annotator=None, diaObjectId=None, fltr=None):
+    """run_query. Two cases here:
+    if annotator=None, runs the query against the local database
+    if annotator and diaObjectId, checks if the query involves the annotator,
+        and if so, runs the query for the given object on main database
         runs the query against the local database
 
     Args:
@@ -142,12 +134,12 @@ def run_query(query, msl):
     sqlquery_real = query['real_sql']
 
 # special way to run query if there are fresh annotations
-#    if annotator:
-#        # if the annotator does not appear in the query tables, then we don't need to run it
-#        if query['tables'] not in annotator:
-#            return []
-#        # run the query against main for this specific object that has been annotated
-#        sqlquery_real = query_for_object(sqlquery_real, diaObjectId)
+    if annotator:
+        # if the annotator does not appear in the query tables, then we don't need to run it
+        if query['tables'] not in annotator:
+            return []
+        # run the query against main for this specific object that has been annotated
+        sqlquery_real = query_for_object(sqlquery_real, diaObjectId)
 
     # in any case, 10 second timeout and limit the output
     sqlquery_real = ('SET STATEMENT max_statement_time=%d FOR %s LIMIT %d' %
@@ -409,40 +401,38 @@ def filters(fltr):
     ntotal = run_queries(fltr, query_list, ms=ms, nid=nid)
     return ntotal
 
-#def fast_anotation_filters(fltr):
-#    """run_annotation_queries.
-#    Pulls the recent content from the kafka topic 'lsst_annotations' 
-#    Each message has an annotator/topic name, and the diaObjectId that was annotated.
-#    Queries that have that annotator should run against that object
-#    """
-#    try:
-#        query_list = fetch_queries()
-#    except Exception as e:
-#        fltr.log.error("ERROR in filter/run_active_queries.fetch_queries" + str(e))
-#        return None
-#
-#    annotation_list = []
-#    conf = {
-#        'bootstrap.servers':   settings.KAFKA_SERVER,
-#        'group.id':            settings.ANNOTATION_GROUP_ID,
-#        'default.topic.config': {'auto.offset.reset': 'earliest'}
-#    }
-#    streamReader = Consumer(conf)
-#    topic = settings.ANNOTATION_TOPIC
-#    streamReader.subscribe([topic])
-#    while 1:
-#        msg = streamReader.poll(timeout=5)
-#        if msg == None: break
-#        try:
-#            ann = json.loads(msg.value())
-#            annotation_list.append(ann)
-#        except:
-#            continue
-#    streamReader.close()
-#    ntotal = run_queries(fltr, query_list, annotation_list)
-#    return ntotal
+def fast_anotation_filters(fltr):
+    """run_annotation_queries.
+    Pulls the recent content from the kafka topic 'lsst_annotations' 
+    Each message has an annotator/topic name, and the diaObjectId that was annotated.
+    Queries that have that annotator should run against that object
+    """
+    try:
+        query_list = fetch_queries()
+    except Exception as e:
+        fltr.log.error("ERROR in filter/run_active_queries.fetch_queries" + str(e))
+        return None
 
-
+    annotation_list = []
+    conf = {
+        'bootstrap.servers':   settings.KAFKA_SERVER,
+        'group.id':            settings.ANNOTATION_GROUP_ID,
+        'default.topic.config': {'auto.offset.reset': 'earliest'}
+    }
+    streamReader = Consumer(conf)
+    topic = settings.ANNOTATION_TOPIC
+    streamReader.subscribe([topic])
+    while 1:
+        msg = streamReader.poll(timeout=5)
+        if msg == None: break
+        try:
+            ann = json.loads(msg.value())
+            annotation_list.append(ann)
+        except:
+            continue
+    streamReader.close()
+    ntotal = run_queries(fltr, query_list, annotation_list)
+    return ntotal
 
 # if __name__ == "__main__":
 #     from src import slack_webhook
