1307b57a8222d42375fc8fe36962b5843707e372
Lasair <lasair@lsst.ac.uk>
2026-03-06 10:52:00 +0000

    add cronjob

--- services/mysqlbackup-web.py
+++ services/mysqlbackup-web.py
@@ -13,12 +13,12 @@ logfile = settings.SERVICES_LOG +'/'+ today + '.log'
 cmd = 'echo "\\n-- mysql backup (web) at %s"' % now
 execute_cmd(cmd, logfile)
 
-cmd = f"mysqldump --user={settings.DB_USER_READWRITE} --password={settings.DB_PASS_READWRITE} \
+cmd = f"mysqldump --single-transaction --user={settings.DB_USER_READWRITE} --password={settings.DB_PASS_READWRITE} \
         --port={settings.BACKUP_DATABASE_PORT} --host={settings.BACKUP_DATABASE_HOST} {settings.DB_DATABASE} \
         annotators areas auth_group auth_group_permissions auth_permission auth_user auth_user_groups \
         auth_user_user_permissions authtoken_token django_admin_log django_content_type django_migrations \
-        django_session lasair_statistics  mma_areas myqueries users_profile watchlist_cones watchlists \
-        > {settings.MYSQL_BACKUP_DIR}/mysqlbackup.sql"
+        django_session lasair_statistics mma_areas myqueries users_profile watchlist_cones watchlists \
+        > {settings.MYSQL_BACKUP_DIR}/mysqlbackup-web.sql"
 execute_cmd(cmd, logfile)
 
 cmd = 'ls -l %s' % settings.MYSQL_BACKUP_DIR
