4818dcc1ede7eddb42474023ba380da565342b38
Roy Williams <roy@roe.ac.uk>
2026-03-08 08:30:13 +0000

    update

--- utility/annotationStatistics.py
+++ utility/annotationStatistics.py
@@ -6,17 +6,6 @@ sys.path.append('../common')
 from src import date_nid
 import src.db_connect as db_connect
 
-def bytes_out(cursor, query_name):
-    # How many bytes output on public Kafka by this filter, for the last 7 days
-    nid = date_nid.nid_now()
-    query = 'SELECT name,value,nid FROM lasair_statistics WHERE nid>%d AND name LIKE "%%%s_bytes%%"'
-    query = query % (nid-7, query_name)
-    cursor.execute(query)
-    print('** Megabytes in %s' % query_name)
-    print(' date    Mbytes')
-    for row in cursor:
-        print('%s %10.0f' % (date_nid.nid_to_date(row['nid']), (row['value']/1000000)))
-
 def annotations_in(cursor, ann_name):
     # How many annotations contributed by this annotator, for the last 7 days
     query = "SELECT count(*) AS nann, DATE(timestamp) as date FROM annotations "
@@ -24,10 +13,15 @@ def annotations_in(cursor, ann_name):
     query += "GROUP BY DAY(timestamp), MONTH(timestamp), YEAR(timestamp)"
     query = query % ann_name
     cursor.execute(query)
-    print('\n** Annotations in %s' % ann_name)
+    print('\n** Annotations ingested for %s' % ann_name)
     print(' date    number')
     for row in cursor:
         print('%s %8d' % (row['date'], row['nann']))
+
+    query = f'SELECT active FROM annotators WHERE topic="{ann_name}"'
+    cursor.execute(query)
+    for row in cursor:
+        return row['active']
 
 def fast_filters(cursor, ann_name):
     # List of active filters that are immediately run as a result of the fast annotator
@@ -35,15 +29,48 @@ def fast_filters(cursor, ann_name):
     query += "WHERE tables LIKE '%%%s%%' and active > 0 ORDER BY last_name"
     query = query % ann_name
     cursor.execute(query)
-    print('\n** Fast annotation filters from %s' % ann_name)
+    print('\n** Fast annotation filters resulting from %s' % ann_name)
     print('   Last name  active  Filter name')
     for row in cursor:
         print('%15s %d %s' % (row['last_name'], row['active'], row['name']))
 
+def bytes_out(cursor, filter_name):
+    # How many bytes output on public Kafka by this filter, for the last 7 days
+    nid = date_nid.nid_now()
+    query = 'SELECT name,value,nid FROM lasair_statistics WHERE nid>%d AND name LIKE "%%%s_bytes%%"'
+    query = query % (nid-7, filter_name)
+    cursor.execute(query)
+    print('\n** Bytes out by Kafka to  %s' % filter_name)
+    print(' date    Mbytes')
+    for row in cursor:
+        date = date_nid.nid_to_date(row['nid'])
+        bytes = f'{int(row["value"]):,}'
+        print('%s %20s' % (date, bytes))
+
 if __name__ == "__main__":
+    import os, sys
+    feeder_name = None
+    if len(sys.argv) > 1:
+        ann_name    = sys.argv[1]
+    if len(sys.argv) > 2:
+        feeder_name = sys.argv[2]
+    if len(sys.argv) == 1:
+        print('Usage: python3 annotationStatistics.py <annotation_name> <feeder_name>')
+        print('examples:')
+        print('  python3 annotationStatistics.py r0b_lvra lvra_fodder')
+        print('  python3 annotationStatistics.py NEEDLE_LSST needle_input_filter_test')
+        sys.exit()
+
     msl = db_connect.remote()
     cursor  = msl.cursor(buffered=True, dictionary=True)
-    bytes_out     (cursor, 'lvra-fodder')
-    annotations_in(cursor, 'r0b_lvra')
-    fast_filters  (cursor, 'r0b_lvra')
+
+    active = annotations_in(cursor, ann_name)
+    if active  == 1:
+        print('\nNORMAL ANNOTATOR')
+    else:
+        print('\nFAST ANNOTATOR')
+        fast_filters  (cursor, ann_name)
+
+    if feeder_name:
+        bytes_out     (cursor, feeder_name)
 
