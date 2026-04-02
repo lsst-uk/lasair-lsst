2080b68d7ab433e11b44eb1d57e4f5a465939f2e
G Francis <g.francis@epcc.ed.ac.uk>
2026-04-02 14:46:41 +0100

    add digest

--- services/email_digest.py
+++ services/email_digest.py
@@ -14,22 +14,45 @@ from docopt import docopt
 from src import db_connect
 from confluent_kafka import Consumer
 import settings
+from time import sleep
 
 
 args = docopt(__doc__)
 
 consumer_conf = {
-    'bootstrap.servers': '',
+    'bootstrap.servers': settings.PUBLIC_KAFKA_READONLY,
     'default.topic.config': {'auto.offset.reset': 'earliest'},
-    'client.id': 'client-1',
-    'group.id': '',
-    'enable.auto.commit': False,
+    'client.id': 'email_digest',
+    'group.id': 'email_digest',
+    'enable.auto.commit': True,
 }
 
+# Get a list of filters
 msl = db_connect.remote()
 cursor = msl.cursor(buffered=True, dictionary=True)
-query = f"SELECT topic_name, first_name, last_name, email FROM myqueries, auth_user WHERE auth_user.id=user AND active=1"
+query = "SELECT name, topic_name, first_name, last_name, email FROM myqueries, auth_user WHERE auth_user.id=user AND active=1"
 cursor.execute(query)
-for row in cursor:
-    print(row)
+filters = cursor.items()
 
+for f in filters:
+    # Get any new alerts
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe([f['topic_name']])
+    alerts = []
+    for i in range(10):
+        msg = consumer.poll(timeout=1)
+        if msg is None:
+            # no messages available
+            sleep(1)
+            continue
+        if msg.error():
+            print('ERROR polling for alerts: ' + str(msg.error()))
+            break
+        alerts.append(msg.value())
+    consumer.close()
+
+    # Create digest email
+    if len(alerts) > 0:
+        digest = f"Lasair alert digest for filter {f['name']}\n\n"
+        for alert in alerts:
+            digest += alert + '\n'
