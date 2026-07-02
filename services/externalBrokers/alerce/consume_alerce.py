9e084a2bd437ae526c1f1ffda3c6af2fd82137f3
Roy Williams <roy@roe.ac.uk>
2026-07-02 10:55:03 +0000

    alerce

--- services/externalBrokers/alerce/consume_alerce.py
+++ services/externalBrokers/alerce/consume_alerce.py
@@ -13,13 +13,20 @@ sys.path.append('../../../common')
 import settings
 
 def load_json_from_web(url):
-  response = urlopen(url)
-  data_json = json.loads(response.read())
-  return data_json
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    return data_json
 
-# light curve classifier is schemaless so need to fetch schema first
-schema_url = "https://raw.githubusercontent.com/alercebroker/pipeline/main/schemas/lc_classification_step/output_ztf.avsc"
-schema = load_json_from_web(schema_url)
+def load_json_from_file(filename):
+    data_json = json.loads(open(filename).read())
+    return data_json
+
+# need to fetch schema first
+#schema_url = "https://raw.githubusercontent.com/alercebroker/pipeline/main/schemas/lc_classification_step/output_ztf.avsc"
+#schema = load_json_from_web(schema_url)
+
+schema_filename = 'stamp_classifier_rubin.avsc'
+schema = load_json_from_file(schema_filename)
 
 def make_stamp_annotation(record):
     r = {}
@@ -30,59 +37,39 @@ def make_stamp_annotation(record):
         if v > maxprob:
             r['classification'] = k
             maxprob = v
-    r['objectId']       = record['objectId']
+    r['diaObjectId'] = record['diaObjectId']
     r['classdict']      = classdict
     if r['classification'] in ['VS', 'AGN', 'asteroid', 'bogus']:
         return None
     else:
         return r
 
-def make_lc_annotation(record):
-    r = {}
-    r['objectId'] = record['oid']
-    lcc = record['lc_classification']
-    r['classification'] = lcc['class']
-    classdict = {}
-    for k,v in lcc['probabilities'].items():
-        if v > 0.02:
-            classdict[k] = float('%.3f'%v)
-    r['classdict'] = classdict
-    if r['classification'] in ['E', 'Periodic-Other']:
-        return None
-    else:
+def handle_deserealized_record(raw_message, topic):
+    bytes_io = io.BytesIO(raw_message.value())
+
+    # Stamp classifier Rubin is a schemaless abro. Give schema to read.
+    # Reader returns a dict.
+    if "stamp_classifier" in topic:
+        try:
+            reader = fastavro.schemaless_reader(bytes_io, schema)
+        except:
+            print(f'Cannot open {topic}')
+            return 'fail'
+        r = make_stamp_annotation(reader)
         return r
 
-def handle_deserealized_record(raw_message, topic):
-  bytes_io = io.BytesIO(raw_message.value())
-
-  # Stamp classifier is a normal avro. Read as usual.
-  # Reader returns an iterator with one message
-  if "stamp_classifier" in topic:
-    reader = fastavro.reader(bytes_io)
-    record = next(reader)
-    r = make_stamp_annotation(record)
-    return r
-
-  # LC Classifier is a schemaless abro. Give schema to read.
-  # Reader returns a dict.
-  elif "lc_classifier_ztf" in topic:
-    reader = fastavro.schemaless_reader(bytes_io, schema)
-    record = reader
-    r = make_lc_annotation(record)
-    return r
-
-  else:
-    raise Exception(f"No schema loaded for topic {topic}")
-    return 0
+    else:
+      raise Exception(f"No schema loaded for topic {topic}")
+      return None
 
 def connect():
     # create a streamReader
     conf = {
         'bootstrap.servers': settings.ALERCE_KAFKA,
-        'group.id'         : settings.ALERCE_GROUP_ID,
+        'group.id'         : settings.ALERCE_NAME + '-001',
         'security.protocol': 'SASL_SSL',
         'sasl.mechanism'   : 'SCRAM-SHA-512',
-        'sasl.username'    : 'lasair',
+        'sasl.username'    : settings.ALERCE_NAME,
         'sasl.password'    : settings.ALERCE_PASSWORD,
         'auto.offset.reset': 'earliest',
     }
@@ -110,27 +97,28 @@ else:
     classifier_type = topic.split('_')[0]
     if classifier_type == 'stamp':
         make_annotation = make_stamp_annotation
-    elif classifier_type == 'lc':
-        make_annotation = make_lc_annotation
     else:
         print('Unknown classifier, quitting')
         sys.exit()
 
-    L = lasair.lasair_client(settings.API_TOKEN, endpoint='https://' + settings.LASAIR_URL + '/api')
+    L = lasair.lasair_client(settings.ALERCE_API_TOKEN, endpoint='https://' + settings.LASAIR_URL + '/api')
 
     # consume a topic from a streamReader and call handle
     streamReader.subscribe([topic])
     nalert = 0
-    while nalert < 5000:  # just do 5000 at a time for now
+    while nalert < 50000:
         msg = streamReader.poll(timeout=20)
         if msg == None:
             break
         r = handle_deserealized_record(msg, topic)
+        if r == 'fail':
+            break
         if r:
+            print(r)
             nann += 1
             L.annotate(
                 'alerce_' + classifier_type,
-                r['objectId'],
+                r['diaObjectId'],
                 r['classification'],
                 version='0.1',
                 explanation='',
@@ -139,5 +127,4 @@ else:
 
         nalert += 1
 streamReader.close()
-if nann > 0:
-    print('\n-- %d annotations from alerce_%s at %s' % (nann, classifier_type, datetime.datetime.now()))
+print('-- %d of %d from alerce_%s at %s' % (nann, nalert, classifier_type, datetime.datetime.now()))
