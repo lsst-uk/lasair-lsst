a0ea4b85873bd9b066d11cc205914fe6d592342b
G Francis <g.francis@epcc.ed.ac.uk>
2026-07-21 18:42:18 +0100

    add a test

--- tests/unit/common/src/test_annotate_util.py
+++ tests/unit/common/src/test_annotate_util.py
@@ -1,17 +1,25 @@
 import context
 import annotate_util
 import unittest
+import unittest.mock as mock
 from unittest.mock import MagicMock
 
 
 class AnnotateUtilTest(unittest.TestCase):
     """Tests for annotation utils."""
 
-    def test_insert_annotations_kafka(self):
-        self.assertTrue(False)  # not implemented
+    @mock.patch('annotate_util.Producer')
+    def test_insert_annotation_kafka(self, mock_producer):
+        annotate_util.lasair_settings.ANNOTATION_TOPIC = 'asdf'
+        annotate_util.insert_annotation_kafka(123, 'test_topic', 'test_class', 'v1', 'expl', '{}', 'test_url')
+        mock_producer.return_value.produce.assert_called_once_with('asdf',
+            '{"diaObjectId": 123, "topic": "test_topic", "classification": "test_class", "version": "v1", '
+            '"explanation": "expl", "classdict": "{}", "url": "test_url"}')
+        mock_producer.return_value.flush.assert_called_once()
 
-    def test_insert_annotation_kafka(self):
-        self.assertTrue(False)  # not implemented
+    def test_insert_annotations_kafka(self):
+        # not required as tested by above
+        pass
 
     def test_insert_annotation_db(self):
         self.assertTrue(False)  # not implemented
