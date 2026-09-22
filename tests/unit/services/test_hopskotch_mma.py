c75bc7763ba0aa1b5f4b2f88c0dc3e1ecb955542
Roy Williams <roy@roe.ac.uk>
2026-09-22 13:09:16 +0000

    update

--- tests/unit/services/test_hopskotch_mma.py
+++ tests/unit/services/test_hopskotch_mma.py
@@ -2,11 +2,12 @@ import sys
 import json
 import context
 import unittest.main
+from pathlib import Path
 from unittest import TestCase
 sys.path.append('../../../../common')
 import settings
 
-sys.path.append('../../../../services/externalBrokers/mma_hopskotch')
+sys.path.append('../../../services/externalBrokers/mma_hopskotch')
 import readLVK, readIcecube
 
 class MmaHopskotchTest(TestCase):
@@ -20,21 +21,44 @@ class MmaHopskotchTest(TestCase):
         '--contours'   : '10,50,90',
     }
     ret = readLVK.handleDataDict(dataDict, options, logger=None)
-    # assert that every file in /tmp/LVK is identical 
-    # to those in sample_hopskotch_output/LVK
+    dir = '/LVK/MS260814i/20260814T080340_preliminary/'
+    expect_dir = 'sample_hopskotch_output' + dir
+    actual_dir = '/tmp' + dir
+
+    self.assertEqual(
+        Path(expect_dir + '/meta.yaml').read_bytes(),
+        Path(actual_dir + '/meta.yaml').read_bytes(),
+        "Files differ: LVK meta.yaml",
+    )
+    self.assertEqual(
+        Path(expect_dir + '/90.moc').read_bytes(),
+        Path(actual_dir + '/90.moc').read_bytes(),
+        "Files differ: LVK 90.moc",
+    )
 
   def test1_readIcecube(self):
-    dataDict = json.loads(open('sample_hopskotch_input/Icecube.json').read())
-    skymap = open('sample_input/Icecube_hopskotch_skymap.fits', 'rb').read()
-    dataDict['event']['skymap'] = skymap
+    dataDict = json.loads(open('sample_hopskotch_input/icecube.json').read())
+    skymap = open('sample_hopskotch_input/icecube_skymap.fits', 'rb').read()
     options = {
         '--superevents': False,
         '--directory'  : '/tmp/Icecube',
         '--contours'   : '10,50,90',
     }
     ret = readIcecube.handleDataDict(dataDict, options, logger=None)
-    # assert that every file in /tmp/Icecube is identical 
-    # to those in sample_hopskotch_output/Icecube
+    dir = '/Icecube/260801a/final/'
+    expect_dir = 'sample_hopskotch_output' + dir
+    actual_dir = '/tmp' + dir
+
+    self.assertEqual(
+        Path(expect_dir + '/meta.yaml').read_bytes(),
+        Path(actual_dir + '/meta.yaml').read_bytes(),
+        "Files differ: Icecube meta.yaml",
+    )
+    self.assertEqual(
+        Path(expect_dir + '/90.moc').read_bytes(),
+        Path(actual_dir + '/90.moc').read_bytes(),
+        "Files differ: Icecube 90.moc",
+    )
 
 if __name__ == '__main__':
   import xmlrunner
